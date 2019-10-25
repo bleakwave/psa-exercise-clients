04503a7d00a9e5c990fe21f4b63a77f6a1315bc9
janette <janette.emen@gmail.com>
2019-10-25 13:46:46 +0800

    region counts controller

--- src/main/java/com/psa/clients/controllers/ClientController.java
+++ src/main/java/com/psa/clients/controllers/ClientController.java
@@ -3,15 +3,19 @@ package com.psa.clients.controllers;
 import java.util.ArrayList;
 import java.util.List;
 
+import javax.swing.plaf.synth.Region;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 
 import com.psa.clients.dao.jpa.ClientRepository;
 import com.psa.clients.dao.jpa.RegionCountRepository;
 import com.psa.clients.domain.Client;
+import com.psa.clients.domain.RegionCount;
 
 
 @Controller
@@ -37,9 +41,20 @@ public class ClientController {
 		return "client";
 	}
 	
-	@RequestMapping("/api/v1/regions/post_count")
-	public String clients () {
-		return "hello";
+	@RequestMapping("/api/v1/regions/post_count/{id}")
+	public String getRegions (Model model) {
+		List<Region> regions = new ArrayList<>();
+		regionCountRepository.findAll().forEach(region -> regions.add(region));
+		model.addAttribute("regions", regions);
+		return "client-list";
+	}
+	
+	@RequestMapping(method = RequestMethod.POST, value="/api/v1/regions/post_count")
+	public String getRegions (@PathVariable int id, Model model) {
+		RegionCount region = regionCountRepository.findById(id).orElse(null);
+		model.addAttribute("regions", region);
+		return "region";
+	}
 		
 	}
 	
