2ea43f17cdfb3e891b0dfcbf0ef1daf401d6cc97
ahmad <a.bagul@psa.gov.ph>
2019-10-25 10:26:26 +0800

    initial repo

---
+++ src/main/java/com/psa/clients/PsaClientsApplication.java
@@ -0,0 +1,13 @@
+package com.psa.clients;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PsaClientsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PsaClientsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/psa/clients/PsaClientsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.psa.clients;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PsaClientsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
